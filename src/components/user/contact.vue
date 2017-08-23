3a87e0a527e407170966a98845762f2d75cb13bd
Benson <472756921@qq.com>
2017-08-23 09:15:19 +0800

    ''

--- src/components/user/contact.vue
+++ src/components/user/contact.vue
@@ -5,6 +5,7 @@
       <div>年龄：23</div>
       <div>性别：23</div>
       <div>地址：成都</div>
+      <div class="del">删除</div>
     </div>
   </div>
 </template>
@@ -17,9 +18,36 @@
       items =  Array.prototype.slice.call(items);
       items.map((item,i) => {
         item.addEventListener('touchstart', (e) => {
-          let touch = event.targetTouches[0];
-          let left = parseInt(item.style.left);
+          if (e.targetTouches.length == 1) {
+            e.preventDefault(); // 阻止浏览器默认事件，重要
+            let touch = e.targetTouches[0];
+            let x = touch.pageX ;
+            let mr = window.getComputedStyle(item.children[4]).marginRight;
+            let mrV = mr.substring(0, mr.length-2);
+            if (mrV < -10) {
+              item.addEventListener('touchmove', function abc(e2) {
+                e2.preventDefault();
+                if (e2.targetTouches.length == 1) {
+                  let mov = e2.targetTouches[0].pageX - x;
+                  if (mov < Math.abs(mrV)) {
+                    item.children[4].style.marginRight = parseInt(mrV) + parseInt(mov) + 'px';
+                  }
+                }
+              }, false)
+              item.addEventListener('touchend', function abcd(e3) {
+                e3.preventDefault();
+                let mr2 = window.getComputedStyle(item.children[4]).marginRight;
+                let mrV2 = mr2.substring(0, mr2.length-2);
+                if (mrV2 > -20) {
+                  item.children[4].style.marginRight = 0;
+                } else {
+                  item.children[4].style.marginRight = mrV;
+                }
+              }, false)
+            }
+          }
         }, false)
+
       })
     },
   };
@@ -34,5 +62,17 @@
     background: #EFF2F7;
     border-bottom: 1px solid #E5E9F2;
     font-size: .8rem;
+    position: relative;
+    overflow: hidden;
+  }
+  .del{
+    position: absolute;
+    background: red;
+    right: 0;
+    top: 0;
+    padding: 2.5rem 1.5rem;
+    color: #fff;
+    font-size: 1rem;
+    margin-right: -5rem;
   }
 </style>
